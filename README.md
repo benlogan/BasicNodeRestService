099645467c54560e34ce4a7e01161c9bef48759e
Ben Logan <benlogan1981@gmail.com>
2018-10-12 14:45:10 +0100

    filter & sort functions

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Serves static JSON file using Express
 
 start;
 node app.js
+
+Then query me like this;
+http://localhost:1337/transcriptlytics?ticker=IBM.N
--- app.js
+++ app.js
@@ -1,19 +1,39 @@
+var url = require('url'); // just for parsing request
 var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 1337;
 
-app.get('/dataSearch', function(request, response) {
-    //var queryData = url.parse(request.url, true).query;
-    //serveJson(queryData.q, response);
-    serveJson(response);
+app.get('/transcriptlytics', function(request, response) {
+  var queryData = url.parse(request.url, true).query;
+  serveJson(queryData.ticker, response);
 });
 
-function serveJson(response) {
-  var parsedJSON = require('./data/data.json');
-  response.end(JSON.stringify(parsedJSON));
+function serveJson(ticker, response) {
+  var parsedJSON = require('./data/transcriptlytics.json');
+
+  if(ticker) {
+    // all results where ticker matches
+    var newArray = parsedJSON.filter(function (el) {
+      return el.ticker == ticker;
+    });
+
+    // date/time ordered
+    newArray.sort(custom_sort);
+
+    // return the filtered/sorted array as JSON
+    response.end(JSON.stringify(newArray));
+  } else {
+    // return all
+    response.end(JSON.stringify(parsedJSON));
+  }
+}
+
+// date ordering sort function
+function custom_sort(a, b) {
+    return new Date(a.transcript_datetime).getTime() - new Date(b.transcript_datetime).getTime();
 }
 
 app.listen(port, function () {
-    console.log('Basic Node REST Server - listening on port ' + port);
+  console.log('Basic Node REST Server - listening on port ' + port);
 });
